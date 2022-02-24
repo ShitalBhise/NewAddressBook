f31307291d28a3ba0f2975c6e0c07ed3c8e74e0f
ShitalBhise <bhiseshital27059@gmail.com>
2022-02-24 22:32:55 +0530

    Logic for adding person

--- src/book/AddressBook.java
+++ src/book/AddressBook.java
@@ -2,7 +2,103 @@ package book;
 
 public class AddressBook {
 
-	public static void main(String[] args) {
-		System.out.println("Welcome to Address Book Program in Address Book");
+	private String fname;
+	private String lname;
+	private String city;
+	private String state;
+	private String zip;
+	private String address;
+	private String phoneNo;
+	private String email;
+
+	public AddressBook(String fname, String lname, String city, String state, String zip, String address,
+			String phoneNo, String email) {
+		this.fname = fname;
+		this.lname = lname;
+		this.city = city;
+		this.state = state;
+		this.zip = zip;
+		this.address = address;
+		this.phoneNo = phoneNo;
+		this.email = email;
 	}
+
+	/*
+	 * Use setters
+	 */
+	public void setfname(String fname) {
+		this.fname = fname;
+	}
+
+	public void setlname(String lname) {
+		this.lname = lname;
+	}
+
+	public void setcity(String city) {
+		this.city = city;
+	}
+
+	public void setstate(String state) {
+		this.state = state;
+	}
+
+	public void setzip(String zip) {
+		this.zip = zip;
+	}
+
+	public void setaddress(String address) {
+		this.address = address;
+	}
+
+	public void setphoneNo(String phoneNo) {
+		this.phoneNo = phoneNo;
+	}
+
+	public void setemail(String email) {
+		this.email = email;
+	}
+
+	/*
+	 * Use getters
+	 */
+	public String getfname() {
+		return fname;
+	}
+
+	public String getlname() {
+		return lname;
+	}
+
+	public String getcity() {
+		return city;
+	}
+
+	public String getstate() {
+		return state;
+	}
+
+	public String getzip() {
+		return zip;
+	}
+
+	public String getaddress() {
+		return address;
+	}
+
+	public String getphoneNo() {
+		return phoneNo;
+	}
+
+	public String getemail() {
+		return email;
+	}
+
+	@Override
+	public String toString() {
+		return "AddressBook [fname=" + fname + ", lname=" + lname + ", city=" + city + ", state=" + state + ", zip="
+				+ zip + ", address=" + address + ", phoneNo=" + phoneNo + ", email=" + email + "]";
+	}
+
+	// public static void main(String[] args) {
+	// System.out.println("Welcome to Address Book Program in Address Book");
 }
---
+++ src/book/AddressbookMain.java
@@ -0,0 +1,100 @@
+package book;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class AddressbookMain {
+
+	static Scanner scanner = new Scanner(System.in);
+	ArrayList<AddressBook> person = new ArrayList<>();
+
+	public void addPerson() {
+		System.out.println("Enter the First Name");
+		String fName = scanner.next();
+		System.out.println("Enter the Last Name");
+		String lName = scanner.next();
+		System.out.println("Enter the Address");
+		String address = scanner.next();
+		System.out.println("Enter the City");
+		String city = scanner.next();
+		System.out.println("Enter the State");
+		String state = scanner.next();
+		System.out.println("Enter the Zip");
+		String zip = scanner.next();
+		System.out.println("Enter the PhoneNumber");
+		String phoneNo = scanner.next();
+		System.out.println("Enter the Email");
+		String email = scanner.next();
+
+//creating Addressbook object and passing arguments inside constructor
+		AddressBook p = new AddressBook(fName, lName, city, state, zip, address, phoneNo, email);
+//adding p object into the linked list with Addressbook class data type
+		person.add(p);
+//printing person array list
+		System.out.println(p);
+
+	}
+
+	// Edit AddressBook details using person name
+
+	/// editing contact by searching names
+	public void editPerson() {
+		System.out.println("Enter the name to edit");
+		String s = scanner.next();
+
+		for (int i = 0; i < person.size(); i++) {
+			AddressBook p = person.get(i);
+			if (s.equals(p.getfname())) {
+				while (true) {
+					System.out.println("Enter choice to edit 1)firstName\n2)lastName\n3)city\n"
+							+ "4)state\n5)zip\n6)address\n7)phoneNo\n8)email\n9)exit");
+					int choice = scanner.nextInt();
+					switch (choice) {
+					case 1:
+						p.setfname(scanner.next());
+						break;
+					case 2:
+						p.setlname(scanner.next());
+						break;
+					case 3:
+						p.setcity(scanner.next());
+						break;
+					case 4:
+						p.setstate(scanner.next());
+						break;
+					case 5:
+						p.setzip(scanner.next());
+
+						break;
+					case 6:
+						p.setaddress(scanner.next());
+						break;
+					case 7:
+						p.setphoneNo(scanner.next());
+						break;
+					case 8:
+						p.setemail(scanner.next());
+						break;
+					default:
+						System.out.println("select correct choice");
+						break;
+					}// end of switch
+					if (choice == 9)
+						break;
+				} // end while
+				person.set(i, p);
+				System.out.println("person after editing");
+				System.out.println(person);
+
+			} // end of if
+		} // end of for loop
+
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		AddressbookMain main = new AddressbookMain();
+		main.addPerson();
+	}
+
+}
