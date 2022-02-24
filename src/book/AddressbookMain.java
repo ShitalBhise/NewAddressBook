00d27e9ba225373410cb4bc041152f37d02016da
ShitalBhise <bhiseshital27059@gmail.com>
2022-02-24 23:16:38 +0530

    Edit person logic

--- src/book/AddressbookMain.java
+++ src/book/AddressbookMain.java
@@ -91,10 +91,65 @@ public class AddressbookMain {
 
 	}
 
+	/// editing contact by searching names
+	public void editPerson1() {
+		System.out.println("Enter the name to edit");
+		String s = scanner.next();
+
+		for (int i = 0; i < person.size(); i++) {
+			AddressBook p = person.get(i);
+			if (s.equals(p.getfname())) {
+				while (true) {
+					System.out.println("Enter choice to edit 1)firstName\n2)lastName\n3)city\n"
+							+ "4)state\n5)zip\n6)address\n7)phoneNo\n8)email\n9)exit");
+					int choice = scanner.nextInt();
+					switch (choice) {
+					case 1:
+						p.setfname(scanner.next());
+						break;
+					case 2:
+						p.setlname(scanner.next());
+						break;
+					case 3:
+						p.setcity(scanner.next());
+						break;
+					case 4:
+						p.setstate(scanner.next());
+						break;
+					case 5:
+						p.setzip(scanner.next());
+
+						break;
+					case 6:
+						p.setaddress(scanner.next());
+						break;
+					case 7:
+						p.setphoneNo(scanner.next());
+						break;
+					case 8:
+						p.setemail(scanner.next());
+						break;
+					default:
+						System.out.println("select correct choice");
+						break;
+					}// end of switch
+					if (choice == 9)
+						break;
+				} // end while
+				person.set(i, p);
+				System.out.println("person after editing");
+				System.out.println(person);
+
+			} // end of if
+		} // end of for loop
+
+	}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		AddressbookMain main = new AddressbookMain();
 		main.addPerson();
+		main.editPerson1();
 	}
 
 }
